e3ed8a914ff13e27fb5c36681af45d95f4f5d38e
Kevin Chen <kevinmichaelchen@gmail.com>
2020-05-12 20:14:43 -0400

    Clients can configure their commit interval

--- kafka.go
+++ kafka.go
@@ -3,21 +3,36 @@ package kafka
 import (
 	"context"
 	"fmt"
+	"time"
+
+	"github.com/segmentio/kafka-go"
 )
 
 type ProcessKafkaMessagesInput struct {
-	Brokers []string
-	GroupID string
-	Topic   string
+	Brokers        []string
+	GroupID        string
+	Topic          string
+	CommitInterval time.Duration
 }
 
 func ProcessKafkaMessages(ctx context.Context, in ProcessKafkaMessagesInput, fn func(context.Context, Message) error) error {
-	reader, closer := getReader(getReaderInput{
-		brokers: in.Brokers,
-		groupID: in.GroupID,
-		topic:   in.Topic,
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers: in.Brokers,
+
+		// If GroupID is specified, then Partition should NOT be specified
+		GroupID: in.GroupID,
+
+		Topic: in.Topic,
+
+		// WatchForPartitionChanges is used to inform kafka-go that a consumer group should be
+		// polling the brokers and rebalancing if any partition changes happen to the topic.
+		WatchPartitionChanges: true,
+
+		// CommitInterval indicates the interval at which offsets are committed to
+		// the broker.
+		CommitInterval: in.CommitInterval,
 	})
-	defer closer()
+	defer reader.Close()
 
 	for {
 		// FetchMessage does not commit offsets automatically when using consumer groups.
@@ -31,8 +46,12 @@ func ProcessKafkaMessages(ctx context.Context, in ProcessKafkaMessagesInput, fn 
 			return fmt.Errorf("failed to process kafka message: %w", err)
 		}
 
-		if err := reader.CommitMessages(ctx, m); err != nil {
-			return fmt.Errorf("failed to commit kafka offset: %w", err)
+		if in.CommitInterval == 0 {
+			// A commit represents the instruction of publishing an update of the last
+			// offset read by a program for a topic and partition.
+			if err := reader.CommitMessages(ctx, m); err != nil {
+				return fmt.Errorf("failed to commit kafka offset: %w", err)
+			}
 		}
 	}
 }
