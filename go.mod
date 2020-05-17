e1a05d69676581b9ed96e7b9f0953c5b72c4921e
Kevin Chen <kevinmichaelchen@gmail.com>
2020-05-17 15:40:33 -0400

    Create message from TraceInfo, EventInfo, and protobuf

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/AlpacaLabs/go-kafka
 go 1.14
 
 require (
+	github.com/AlpacaLabs/protorepo-event-go v0.0.0-20200517012157-817122666c68
 	github.com/golang/protobuf v1.4.1
 	github.com/segmentio/kafka-go v0.3.6
 )
--- message.go
+++ message.go
@@ -3,13 +3,50 @@ package kafka
 import (
 	"fmt"
 
+	eventV1 "github.com/AlpacaLabs/protorepo-event-go/alpacalabs/event/v1"
+
 	"github.com/golang/protobuf/proto"
 	"github.com/segmentio/kafka-go"
+)
+
+const (
+	HeaderForTraceID = "trace_id"
+	HeaderForEventID = "event_id"
 )
 
 type Message struct {
 	Headers map[string][]byte
 	Payload []byte
+}
+
+func NewMessage(traceInfo eventV1.TraceInfo, eventInfo eventV1.EventInfo, pb proto.Message) (Message, error) {
+	m := Message{
+		Headers: make(map[string][]byte),
+	}
+	b, err := proto.Marshal(pb)
+	if err != nil {
+		return Message{}, err
+	}
+	m.Payload = b
+
+	m.Headers[HeaderForTraceID] = []byte(traceInfo.TraceId)
+	m.Headers[HeaderForEventID] = []byte(eventInfo.EventId)
+
+	return m, nil
+}
+
+func (m Message) ToKafkaMessage() kafka.Message {
+	var headers []kafka.Header
+	for k, v := range m.Headers {
+		headers = append(headers, kafka.Header{
+			Key:   k,
+			Value: v,
+		})
+	}
+	return kafka.Message{
+		Headers: headers,
+		Value:   m.Payload,
+	}
 }
 
 func fromMessage(in kafka.Message) Message {
